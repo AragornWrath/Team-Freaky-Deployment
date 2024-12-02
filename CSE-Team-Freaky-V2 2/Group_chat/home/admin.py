562a78662d16e1796a7c395cf2765b96d0567bd4
AragornWrath <73181347+AragornWrath@users.noreply.github.com>
2024-12-02 03:55:03 -0500

    Delete CSE-Team-Freaky-V2 2/Group_chat directory

--- CSE-Team-Freaky-V2 2/Group_chat/home/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import TripItem
-
-admin.site.register(TripItem)
-import home.models
-# Register your models here.
-
-admin.site.register(home.models.userModel)
