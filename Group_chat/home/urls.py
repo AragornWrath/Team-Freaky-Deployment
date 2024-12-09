badc06eddd0d9e17bc262443f23765d589fb7004
wthobika <wthobika@buffalo.edu>
2024-12-08 21:55:16 -0500

    reverted

--- Group_chat/home/urls.py
+++ Group_chat/home/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path("trips/", views.index_trips, name="trips"),
-    path("tasks/", views.index_tasks, name="tasks"),
     path('serveRegister/', views.serveRegister, name='serverRegister'),
     path('serveLogin/', views.serveLogin, name='serveLogin'),
     path('serveLoginFailed/', views.serveLoginFailed, name='serveLogin'),
@@ -18,8 +17,6 @@ urlpatterns = [
     path('all_trips/delete-like', views.delete_like, name="deleteLike"),
     path('all_trips/view-likes', views.view_likes, name="viewLikes"),
     path('media-uploads/<str:trip_id>', views.uploadImage, name='updateGallery'),
-    path('trips/add-friend/<str:trip_id>', views.add_friend, name='updateGallery'),
-    path('trips/add-task/<str:trip_id>', views.add_task, name='updateGallery'),
     path('trips/<str:trip_id>',views.load_trip_by_id,name='tripByID'),
     # path('root/home/userImages/<str:trip_id>', views.serveMedia, name='')
 ]
